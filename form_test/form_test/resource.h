594278c1cd4549e600064d6ea0cad8a00eb2dafe
jeffharris88 <jeffharris88@accf1826-6565-04f0-df9b-893c0ca74f04>
2010-04-15 07:31:14 +0000

    added menu item to create defaultLabels and actually does it too

--- form_test/form_test/resource.h
+++ form_test/form_test/resource.h
@@ -34,13 +34,14 @@
 #define ID_ADDBOOK_AUTHOR               32779
 #define ID_AUTHOR_REMOVEBOOK            32780
 #define ID_REMOVEBOOK_AUTHOR            32781
+#define ID_LABELS_MAKEDEFAULTLABELS     32782
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        133
-#define _APS_NEXT_COMMAND_VALUE         32782
+#define _APS_NEXT_COMMAND_VALUE         32783
 #define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
